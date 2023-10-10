7fa057f9d3f8142b6a7d978f4eea611ce4c010e6
Johannes Wielinga <johannes.wielinga@bettyblocks.com>
2023-10-10 18:08:07 +0200

    fix: ui background fix

--- src/components/History/Main/Main.tsx
+++ src/components/History/Main/Main.tsx
@@ -3,8 +3,8 @@ import { List } from "../List";
 
 export function Main() {
   return (
-    <div className="flex flex-col rounded-md shadow-lg w-96 min-h-max">
-      <h1 className="flex items-center px-4 py-6 bg-white font-xxl font-bold text-sky-900">
+    <div className="flex flex-col rounded-lg shadow-lg bg-neutral w-96 min-h-max box-content overflow-visible">
+      <h1 className="flex items-center px-4 py-6 bg-white font-xxl font-bold text-sky-900 rounded-t-md">
         List of actions commited
       </h1>
       <List />
