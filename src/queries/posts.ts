5631e3d8e36cf01db62b1850383e497095673a80
Johannes Wielinga <johannes.wielinga@bettyblocks.com>
2023-10-26 15:26:52 +0200

    chore: get 5 posts

--- src/queries/posts.ts
+++ src/queries/posts.ts
@@ -1,4 +1,6 @@
 export const getPosts = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const res = await fetch(
+    "https://jsonplaceholder.typicode.com/posts?_page=0&_limit=5"
+  );
   return res.json();
 };
